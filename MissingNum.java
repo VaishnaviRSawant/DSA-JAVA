a09cc22c7e58e93aa4890487299442696c3da5e5
VaishnaviRSawant <vaishnaviramsawant@gmail.com>
2024-07-21 12:50:05 +0530

    new file

---
+++ MissingNum.java
@@ -0,0 +1,17 @@
+class MissingNum{
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+
+        int naturalSum=0;
+        for(int i=0;i<=n;i++)
+        {
+            naturalSum+=i;
+        }
+        return naturalSum-sum;
+    }
+}
